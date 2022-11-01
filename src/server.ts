a018ec6cd14b9ac6132f52ebf780458386540af1
ubuntu-user <ehsan@say.company>
2022-10-31 22:36:28 -0400

    cors issue

--- src/server.ts
+++ src/server.ts
@@ -36,7 +36,7 @@ async function startServer() {
 
   app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "*");
 
     next();
   });
