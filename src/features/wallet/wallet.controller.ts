33e3281d5e8b048f7e726b9db175d745304996ec
Ehsan <ehsan@say.company>
2023-04-02 09:09:52 -0400

    cookies setup

--- src/features/wallet/wallet.controller.ts
+++ src/features/wallet/wallet.controller.ts
@@ -49,8 +49,8 @@ export class SignatureController {
       session.save();
     }
     res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // res.setHeader('Access-Control-Allow-Credentials', true);
 
     res
       .status(200)
