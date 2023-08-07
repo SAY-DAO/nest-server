a907fa76d7674603e031e7b1127b4f6cdc2807d3
Gbuntu <ehsan@say.company>
2023-08-07 02:52:22 -0400

    session table

--- src/config.ts
+++ src/config.ts
@@ -31,7 +31,7 @@ function loadConfig() {
       password: process.env.DB_PASS ?? 'postgres',
       database: process.env.DB_NAME ?? 'say_dapp',
       enabled: true,
-      synchronize:NODE_ENV === 'development' ? true: false,
+      synchronize:NODE_ENV === 'development' ? true: true,
       logging: true,
       dropSchema: false,
       autoLoadEntities: true,
