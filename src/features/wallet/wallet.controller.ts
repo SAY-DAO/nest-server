42bdf14340bc1d55686017294c1da16ed8963489
Gbuntu <ehsan@say.company>
2023-09-20 11:35:59 +0330

    arrival condition

--- src/features/wallet/wallet.controller.ts
+++ src/features/wallet/wallet.controller.ts
@@ -302,12 +302,12 @@ export class WalletController {
         }
       });
 
-      // if (counter - body.arrivedColumnNumber !== 0) {
-      //   throw new WalletExceptionFilter(
-      //     418,
-      //     'You have to announce arrivals first!',
-      //   );
-      // }
+      if (counter - body.arrivedColumnNumber !== 0) {
+        throw new WalletExceptionFilter(
+          418,
+          'You have to announce arrivals first!',
+        );
+      }
       const flaskNeed = await this.needService.getFlaskNeed(body.flaskNeedId);
       const { need, child } = await this.syncService.syncNeed(
         flaskNeed,
